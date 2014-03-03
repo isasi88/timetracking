0b83ab492cf9755fdb65e6928bebb447d73a6bf9
Kike Isasi <kikeisasi@gmail.com>
2014-03-03 10:38:57 +0000

    Setting up rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Timetracking::Application.config.secret_key_base = 'd37e66d98d13bc20f274392979147c3bdb0a9f7959d55e5a145a141f791b6b7d9b16df510e1d2068da696389a7012a364202ed593677caec943380e2ff9e2104'
