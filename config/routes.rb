4f3f0b25efb1e914cfe85f4ad689c9ecb573aaf0
Kike Isasi <kikeisasi@gmail.com>
2014-03-03 11:33:15 +0000

    Implementing Home Controller and Contact Controller + Routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Timetracking::Application.routes.draw do
+
+get '/' => 'site#home'
+get '/contact' => 'site#contact'
+end
+
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -53,4 +59,3 @@ Timetracking::Application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
-end
